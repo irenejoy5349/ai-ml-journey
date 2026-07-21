1089abb7baa23dccc5a9e130a99ee676814de66c
irenejoy5349 <devisri2026@gmail.com>
2026-07-21 15:49:24 +0530

    Add Data Types program

---
+++ Module-01-Python/02_data_types.py
@@ -0,0 +1,16 @@
+# Data Types in Python
+
+int_age = 24
+float_temperature = 98.2
+string_name = "Priya"
+is_it_raining = True
+
+print(f"Age: {int_age}")
+print(f"Temperature: {float_temperature}")
+print(f"Name: {string_name}")
+print(f"Is it raining: {is_it_raining}")
+
+print(type(int_age))
+print(type(float_temperature))
+print(type(string_name))
+print(type(is_it_raining))
