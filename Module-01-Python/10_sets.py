a4d84f64280e916ad6ee8d78f88deaa778c6baaf
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 16:20:46 +0530

    Add example code for Python sets

---
+++ Module-01-Python/10_sets.py
@@ -0,0 +1,19 @@
+# Sets
+
+numbers = {10, 20, 30, 40}
+
+print(numbers)
+
+print("----------------")
+
+numbers.add(50)
+print(numbers)
+
+print("----------------")
+
+numbers.remove(20)
+print(numbers)
+
+print("----------------")
+
+print(len(numbers))
