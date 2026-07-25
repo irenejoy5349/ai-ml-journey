fa0ea6feb5bbe2ee34fe0865b84979ecdfcab601
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 15:42:24 +0530

    Typecasting

---
+++ Module-01-Python/03_type_casting.py
@@ -0,0 +1,55 @@
+# Type Casting
+
+# String to Integer
+
+num = "10"
+
+print(num)
+print(type(num))
+
+num = int(num)
+
+print(num)
+print(type(num))
+
+print("----------------")
+
+# Integer to Float
+
+age = 22
+
+print(age)
+print(type(age))
+
+age = float(age)
+
+print(age)
+print(type(age))
+
+print("----------------")
+
+# Float to Integer
+
+price = 99.9
+
+print(price)
+print(type(price))
+
+price = int(price)
+
+print(price)
+print(type(price))
+
+print("----------------")
+
+# Integer to String
+
+marks = 95
+
+print(marks)
+print(type(marks))
+
+marks = str(marks)
+
+print(marks)
+print(type(marks))
