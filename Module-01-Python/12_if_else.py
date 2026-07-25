17769ca1362ef94eda58adffa62356952c0af298
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 16:25:42 +0530

    Add if, if-else, and if-elif-else examples

---
+++ Module-01-Python/12_if_else.py
@@ -0,0 +1,32 @@
+# if
+
+age = 20
+
+if age >= 18:
+    print("Eligible to vote")
+
+print("----------------")
+
+# if else
+
+num = 15
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+print("----------------")
+
+# if elif else
+
+marks = 78
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
