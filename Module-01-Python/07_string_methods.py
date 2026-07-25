5573b2a7b26531b9afad17474184f1f58aac9588
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 16:08:51 +0530

    String Methods

---
+++ Module-01-Python/07_string_methods.py
@@ -0,0 +1,20 @@
+# String Methods
+
+text = "python programming"
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+
+print("----------------")
+
+print(text.replace("python", "java"))
+
+print("----------------")
+
+print(text.startswith("python"))
+print(text.endswith("ing"))
+
+print("----------------")
+
+print(text.split())
