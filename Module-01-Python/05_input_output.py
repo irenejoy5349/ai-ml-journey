ccff8ecbda0dd68f15a98688cc079a88e9638119
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 16:02:24 +0530

    Input and Output

---
+++ Module-01-Python/05_input_output.py
@@ -0,0 +1,19 @@
+# Input and Output
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Name:", name)
+print("Age:", age)
+
+print("----------------")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 // num2)
