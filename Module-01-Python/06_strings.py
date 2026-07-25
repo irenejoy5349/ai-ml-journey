ad8554ab3cd88d2eb760b034129bf8aa011aa011
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 16:06:01 +0530

    Strings

---
+++ Module-01-Python/06_strings.py
@@ -0,0 +1,20 @@
+# Strings
+
+name = "Joy"
+
+print(name)
+print(type(name))
+
+print("----------------")
+
+print(name[0])
+print(name[1])
+
+print("----------------")
+
+print(len(name))
+
+print("----------------")
+
+print(name[0:2])
+print(name[1:4])
