d93fc8c9ad647550f81ce5c3fb2f4fdc91bcee49
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 16:30:26 +0530

    Add functions for greeting, addition, and squaring

---
+++ Module-01-Python/14_functions.py
@@ -0,0 +1,26 @@
+# Function without arguments
+
+def greet():
+    print("Hello")
+
+greet()
+
+print("----------------")
+
+# Function with arguments
+
+def add(a, b):
+    print("Sum =", a + b)
+
+add(10, 20)
+
+print("----------------")
+
+# Function with return value
+
+def square(num):
+    return num * num
+
+result = square(5)
+
+print(result)
