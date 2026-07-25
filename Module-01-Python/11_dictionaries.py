a76574911394a23162206c1ef392696696c8e945
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 16:23:20 +0530

    Add student dictionary

---
+++ Module-01-Python/11_dictionaries.py
@@ -0,0 +1,26 @@
+# Dictionary
+
+student = {
+    "name": "Joy",
+    "age": 22,
+    "course": "Python"
+}
+
+print(student)
+
+print("----------------")
+
+print(student["name"])
+print(student["age"])
+
+print("----------------")
+
+student["city"] = "Hyderabad"
+
+print(student)
+
+print("----------------")
+
+print(student.keys())
+
+print(student.values())
