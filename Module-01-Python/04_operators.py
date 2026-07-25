71bc0c48f4cb54b21c6079f401c3ef05fbd94274
irenejoy5349 <devisri2026@gmail.com>
2026-07-25 15:54:11 +0530

    operators

---
+++ Module-01-Python/04_operators.py
@@ -0,0 +1,52 @@
+# Arithmetic Operators
+
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+
+
+# Comparison Operators
+
+x = 20
+y = 15
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+
+# Assignment Operators
+
+num = 10
+
+num += 5
+print(num)
+
+num -= 2
+print(num)
+
+num *= 3
+print(num)
+
+
+
+# Logical Operators
+
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
