e11bf2c48f18bcdcce645f22296a6d92db2cafe4
irenejoy5349 <devisri2026@gmail.com>
2026-07-30 00:26:44 +0530

    Enhance student performance analysis with visualizations
    Added analysis for missing values, duplicate rows, and average score calculations. Included visualizations for study hours vs average score, average score distribution, and feature correlation heatmap.

--- Mini-Projects/Student-Performance-Analysis/analysis.py
+++ Mini-Projects/Student-Performance-Analysis/analysis.py
@@ -13,3 +13,81 @@ print(df.shape)
 print(df.info())
 
 print(df.describe())
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+
+print("\nDuplicate Rows:")
+print(df.duplicated().sum())
+
+df["Average_Score"] = (
+    df["Math_Score"] +
+    df["Science_Score"] +
+    df["English_Score"]
+) / 3
+
+
+print("\nData with Average Score:")
+print(df)
+
+plt.figure(figsize=(6,4))
+
+sns.scatterplot(
+    x="Study_Hours",
+    y="Average_Score",
+    data=df
+)
+
+plt.title("Study Hours vs Average Score")
+
+plt.xlabel("Study Hours")
+
+plt.ylabel("Average Score")
+
+plt.show()
+
+plt.figure(figsize=(6,4))
+
+sns.histplot(
+    df["Average_Score"],
+    bins=5,
+    kde=True
+)
+
+plt.title("Average Score Distribution")
+
+plt.xlabel("Average Score")
+
+plt.ylabel("Number of Students")
+
+plt.show()
+
+plt.figure(figsize=(8,5))
+
+correlation = df.corr(numeric_only=True)
+
+sns.heatmap(
+    correlation,
+    annot=True
+)
+
+plt.title("Feature Correlation Heatmap")
+
+plt.show()
+
+print("\nTop Student:")
+
+top_student = df.loc[df["Average_Score"].idxmax()]
+
+print(top_student)
+
+
+print("\nAverage Score:")
+
+print(df["Average_Score"].mean())
+
+
+print("\nAverage Study Hours:")
+
+print(df["Study_Hours"].mean())
