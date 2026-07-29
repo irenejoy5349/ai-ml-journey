0ee8c6c9b5a966d0ff5df30fc3d5725fc0cb5931
irenejoy5349 <devisri2026@gmail.com>
2026-07-29 23:28:10 +0530

    Add NumPy array examples in 02_numpy_arrays.py
    This file demonstrates the creation and printing of various NumPy arrays, including 1D, 2D, float, and string arrays.

---
+++ Module-02-NumPy/02_numpy_arrays.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+a = np.array([10, 20, 30])
+
+print(a)
+
+b = np.array((1, 2, 3))
+
+print(b)
+
+c = np.array([[1, 2], [3, 4]])
+
+print(c)
+
+d = np.array([1.5, 2.5, 3.5])
+
+print(d)
+
+e = np.array(["apple", "banana", "mango"])
+
+print(e)
