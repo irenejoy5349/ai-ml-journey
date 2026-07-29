2577818385ebee2ed0801c63e698f8f72a9a9011
irenejoy5349 <devisri2026@gmail.com>
2026-07-29 23:48:01 +0530

    Add array indexing examples using NumPy

---
+++ Module-02-NumPy/04_array_indexing.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([10, 20, 30, 40, 50])
+
+print(a[0])
+
+print(a[2])
+
+print(a[-1])
+
+
+b = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+print(b[0, 0])
+
+print(b[1, 2])
