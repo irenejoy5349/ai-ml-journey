0f820796363d66ad11e7a96d603dde67f92e0cf5
irenejoy5349 <devisri2026@gmail.com>
2026-07-29 23:50:54 +0530

    Add array reshaping examples using NumPy

---
+++ Module-02-NumPy/06_array_reshape.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5, 6])
+
+print(a)
+
+b = a.reshape(2, 3)
+
+print(b)
+
+c = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+d = c.reshape(3, 2)
+
+print(d)
